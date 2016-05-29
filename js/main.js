7a55a7b6836c95cabe0832cd1901f551e52b206f
Younji <younjiwoo@gmail.com>
2016-05-28 22:47:51 -0700

    Made a navigation bar responsive with a plug-in.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
-$(document).ready(function() {
-// "When the document is ready, then do the functions inside the braces."
-}); 
-/* this is for jQuery (jQuery is a library). you have to use a different version of that if you only use javascript. window.onload={code in here} */
+
+$(function() {
+    $(".slideShow").responsiveSlides({
+      auto: true,             // Boolean: Animate automatically, true or false
+      speed: 1500,            // Integer: Speed of the transition, in milliseconds
+      timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+      pager: true,           // Boolean: Show pager, true or false
+      nav: true,             // Boolean: Show navigation, true or false
+      random: false,          // Boolean: Randomize the order of the slides, true or false
+      pause: false,           // Boolean: Pause on hover, true or false
+      pauseControls: false,    // Boolean: Pause when hovering controls, true or false
+      prevText: "Previous",   // String: Text for the "previous" button
+      nextText: "Next",       // String: Text for the "next" button
+      maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+      navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+      manualControls: "",     // Selector: Declare custom pager navigation
+      namespace: "rslides",   // String: Change the default namespace used
+      before: function(){},   // Function: Before callback
+      after: function(){}     // Function: After callback
+    });
+});
+    
+    
+    
+$(function() {    // This is same as doc ready function.
+$(".rslides").responsiveSlides();
+});
+
+
+      $(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+
---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+// Younji's Navigation Plug-in................................
+$(document).ready(function() {
+// "When the document is ready, then do the functions inside the braces."
+    $('#nav_ul').smartmenus();
+}); 
