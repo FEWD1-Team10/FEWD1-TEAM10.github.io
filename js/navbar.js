cf6187c7193de55d45b1a978e1df9be99a5a9264
Younji <younjiwoo@gmail.com>
2016-05-31 21:28:29 -0700

    made the navigation bar fade in and fade out with toggle function!

--- js/navbar.js
+++ js/navbar.js
@@ -2,4 +2,8 @@
 $(document).ready(function() {
 // "When the document is ready, then do the functions inside the braces."
     $('#nav_ul').smartmenus();
+    $('#menuIcon').on('click', function(){
+        $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+        $('#nav_ul').fadeToggle(300);
+    });
 }); 
